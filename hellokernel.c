2d9ff9fa8e0b39cb2a3ef58fa0af1d60bc0097c4
Kamila Souckova <coding@kamila.is>
2019-11-26 12:46:20 +0100

    add a sysctl

--- hellokernel.c
+++ hellokernel.c
@@ -3,10 +3,16 @@
 #include <sys/module.h>
 #include <sys/kernel.h>
 #include <sys/systm.h>
+#include <sys/sysctl.h>
 
 /* style(9) says: "Function prototypes for private functions go at the top of
 the first source module." */
 static int load_handler(struct module *module, int event, void *arg);
+
+/* see sysctl(9) */
+static int sysctltest = 42;
+SYSCTL_INT(_debug, OID_AUTO, hello, CTLFLAG_RW, &sysctltest, 0,
+    "Testing a static sysctl");
 
 /* see module(9) */
 static int
@@ -16,12 +22,15 @@ load_handler(struct module *module __attribute__((unused)), int event,
 	switch (event) {
 	case MOD_LOAD:
 		printf("Hello Kernel!\n");
+		printf("(also, sysctltest is %d)\n", sysctltest);
 		return 0;
 	case MOD_QUIESCE:
 		printf("Noooooo!!! I don't wanna leaaave :'(\n");
+		printf("(also, sysctltest is %d)\n", sysctltest);
 		return EBUSY;
 	case MOD_UNLOAD:
 		printf("Okay okay, I'm gone\n");
+		printf("(also, sysctltest is %d)\n", sysctltest);
 		return 0;
 	default:
 		return EOPNOTSUPP;
