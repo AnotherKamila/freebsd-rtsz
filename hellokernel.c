077194d08466b16fd2bf7b9b26f82f42dc9f3114
Kamila Souckova <coding@kamila.is>
2019-11-26 12:12:22 +0100

    style(9) + .gitignore

--- hellokernel.c
+++ hellokernel.c
@@ -1,30 +1,37 @@
+#include <sys/errno.h>
 #include <sys/param.h>
 #include <sys/module.h>
 #include <sys/kernel.h>
 #include <sys/systm.h>
 
-static int event_handler(struct module *module, int event, void *arg);
+/* style(9) says: "Function prototypes for private functions go at the top of
+the first source module." */
+static int load_handler(struct module *module, int event, void *arg);
 
-static int event_handler(struct module *module, int event, void *arg) {
-  switch (event) {
-  case MOD_LOAD:
-    printf("Hello Kernel!\n");
-    return 0;
-  case MOD_QUIESCE:
-    printf("Noooooo!!! I don't wanna leaaave :'(\n");
-    return EBUSY;
-  case MOD_UNLOAD:
-    printf("Okay okay, I'm gone\n");
-    return 0;
-  default:
-    return EOPNOTSUPP;
-  }
+/* see module(9) */
+static int
+load_handler(struct module *module __attribute__((unused)), int event,
+    void *arg __attribute__((unused)))
+{
+	switch (event) {
+	case MOD_LOAD:
+		printf("Hello Kernel!\n");
+		return 0;
+	case MOD_QUIESCE:
+		printf("Noooooo!!! I don't wanna leaaave :'(\n");
+		return EBUSY;
+	case MOD_UNLOAD:
+		printf("Okay okay, I'm gone\n");
+		return 0;
+	default:
+		return EOPNOTSUPP;
+	}
 }
 
 static moduledata_t mod_data = {
-    .name   = "hellokernel",
-    .evhand = event_handler,
-    .priv   = NULL /* TODO what's this? */
+	.name   = "hellokernel",
+	.evhand = load_handler,
+	.priv   = NULL /* TODO what's this? */
 };
 
 DECLARE_MODULE(hellokernel, mod_data, SI_SUB_DRIVERS, SI_ORDER_MIDDLE);
